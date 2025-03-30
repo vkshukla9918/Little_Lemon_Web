7f1a259ab0969725dd514960c6cddee3f0ca3ccb
vkshukla9918 <vkshukla9918@gmail.com>
2025-03-30 16:47:50 +0530

    migration of data to sql

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'restaurant',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -79,12 +80,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydb',
         'USER': 'root',
-        'PASSWORD':'Vivek@2001',
-        'HOST':  '127.0.0.1',  
+        'PASSWORD':'root123',
+        'HOST':  'localhost',  
         'PORT': '3306',  
         'OPTIONS': {  
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
         }  
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -2,13 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Menu(models.Model):
-    ID = models.IntegerField()
     Title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10,decimal_places=2)
-    Inventory = models.IntegerField()
+    Inventory = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.Tittle
 
 class Booking(models.Model):
-    ID = models.IntegerField()
     Name = models.CharField(max_length=255)
-    No_of_guests = models.IntegerField()
+    No_of_guests = models.SmallIntegerField()
     BookingDate = models.DateTimeField()
+
+    def __str__(self):
+        return self.Name
