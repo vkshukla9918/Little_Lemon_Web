4040ee3b445dd38cb9e2b0801b0de5cd3a497d42
vkshukla9918 <vkshukla9918@gmail.com>
2025-04-03 03:00:38 +0530

    Home page

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from restaurant import views
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('api/', include('restaurant.urls')),
     path('auth/', include('djoser.urls')),
